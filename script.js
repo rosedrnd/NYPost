833273f62c99f74275a0a04dce93c93383a66ba8
Catherine Rose <catherinerosedurand@gmail.com>
2020-12-24 17:26:00 +0100

    resolved issues with Jquery

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 $(document).ready(
-    
+
     function () {
         let taux = 0.8;
-        
-        function titi() {
-            taux = taux + 0.1; 
-            $('#nasdaq-value').text(taux);
-            setTimeout(function () {
-                titi(); 
-            }, 5000); 
-        }
-     
-        titi(); 
-    
-        $('.lien-inter').cick(function() {
-            $('.book-to-change').text("Le Podavast de l'Amour moderne");
-            $('.book-to-change').addClass('text-en-rouge');
-        });
-    }    
 
-); 
+        function titi() {
+            taux = taux + 0.1;
+
+            $('#nasdaq-value').text(taux);
+
+            setTimeout(function () {
+                titi();
+            }, 1000);
+        }
+
+        titi();
+
+        $('.lien-inter').click(
+
+            function() {
+
+                $('.book-to-change').text("Le Podcast de l'Amour Moderne");
+                $('.book-to-change').addClass('text-en-rouge');
+
+            }
+
+        );
+    }
+
+);
